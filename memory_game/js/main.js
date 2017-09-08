9ff87bee6d9fe75d33f624ea6cc164fb6d83968c
Tucker Bolding <tuckbolding@Tuckers-MacBook-Pro.local>
2017-09-07 17:00:20 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,19 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+//create cardOne variable
+var cardOne = cards [0];
+cardsInPlay.push ('cardOne');
+//Display card that was flipped
+console.log("User flipped Queen");
+//Create cardTwo variable
+var cardTwo = cards [2];
+cardsInPlay.push ('cardTwo');
+//display card flipped
+console.log("User flipped King")
+
+if (cardsInPlay.length === 2 && cardsInPlay[1]===cardsInPlay[0]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again")
+}
