a83634e5cf676c6c4b8a5d15f484bbe3dc4fe1b4
Tucker Bolding <tuckbolding@Tuckers-MacBook-Pro.local>
2017-09-11 13:32:13 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+	rank: "Queen",
+	suit: "Hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "Queen",
+	suit: "Diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "King",
+	suit: "Hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "King",
+	suit: "Diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 //creat checkForMatch function
 var checkForMatch = function() {
@@ -11,9 +32,11 @@ var checkForMatch = function() {
 }
 //create flipcard function
 var flipCard = function(cardId) {
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 }
 
 flipCard(0);
