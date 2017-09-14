ddae0512a7fa64886dd8894c17a8b636d4bae548
Tucker Bolding <tuckbolding@Tuckers-MacBook-Pro.local>
2017-09-13 18:12:47 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,22 +22,46 @@ var cards = [
 }
 ];
 var cardsInPlay = [];
-//creat checkForMatch function
+
+
 var checkForMatch = function() {
+	
 	if (cardsInPlay[0] === cardsInPlay[1]) {
       alert("You found a match!");
   } else {
       alert("Sorry, try again.");
   }
-}
-//create flipcard function
-var flipCard = function(cardId) {
+};
+
+var flipCard = function() {
+var cardId = this.getAttribute('data-id');
+this.setAttribute('src', cards[cardId].cardImage);
 console.log("User flipped " + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
-}
+};
 
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		var board = document.getElementById('game-board');
+		board.appendChild(cardElement);
+	}
+};
+
+var resetBoard = function() {
+	if (cardsInPlay === 2) {
+   	document.getElementById("game-board").reset();
+	var resetButton = document.getElementById('reset');
+	resetButton.addEventListener('click', resetBoard);
+}
+};
+
+createBoard();
+
